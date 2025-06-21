a821eced4e2c6220d91e1cdcf00461adae184ee3
Theo Dev <theodoredev711@gmail.com>
2025-06-21 22:11:01 +0100

    Update vente.Png to vente.jpeg

--- client/src/components/Project.tsx
+++ client/src/components/Project.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { FaArrowRightLong } from "react-icons/fa6";
 import virtualImg from "../assets/virtual.jpeg";
-import vente from "../assets/vente.png"
+import vente from "../assets/vente.jpeg"
 
 const PROJECTS = [
   {
